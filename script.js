61ea580d0401cfa4268f2097db5610736950710b
Daniela <daanituty@gmail.com>
2024-06-16 21:18:37 -0400

    edicion color

--- script.js
+++ script.js
@@ -15,22 +15,6 @@ function crearAnimaciones() {
         heart.style.animationDelay = Math.random() * 5 + 's';
         document.body.appendChild(heart);
 
-        const cat = document.createElement('div');
-        cat.classList.add('cat');
-        cat.style.backgroundImage = 'url("https://cdn-icons-png.freepik.com/256/6988/6988878.png?semt=ais_hybrid")'; 
-        cat.style.left = Math.random() * window.innerWidth + 'px';
-        cat.style.top = Math.random() * window.innerHeight + 'px';
-        cat.style.animationDelay = Math.random() * 5 + 's';
-        document.body.appendChild(cat);
-
-        const cat2 = document.createElement('div');
-        cat2.classList.add('cat2');
-        cat2.style.backgroundImage = 'url("https://cdn.icon-icons.com/icons2/2787/PNG/512/black_cat_icon_177458.png")';
-        cat2.style.left = Math.random() * window.innerWidth + 'px';
-        cat2.style.top = Math.random() * window.innerHeight + 'px';
-        cat2.style.animationDelay = Math.random() * 5 + 's';
-        document.body.appendChild(cat2);
-
         setTimeout(() => {
             heart.remove();
             cat.remove();
